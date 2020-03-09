c2c67288e610642dd8dd3f3853a3fb0a30dcd462
KostiantynLogunov <777koc@gmail.com>
2020-03-10 01:52:11 +0200

    first commit

---
+++ Models/Products.cs
@@ -0,0 +1,12 @@
+﻿namespace NewRestJWT.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Color { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int AvailableQuantity { get; set; }
+    }
+}
